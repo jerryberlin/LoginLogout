dfc018283bc60dbef04f8da6ded042d5b05cfc61
Jerry <jerryberlin0505@gmail.com>
2022-07-05 10:55:33 +0700

    splash screen, simple animation

--- app/src/main/java/com/example/loginlogout/navigation/Navigation.kt
+++ app/src/main/java/com/example/loginlogout/navigation/Navigation.kt
@@ -4,13 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.loginlogout.presentation.screen.splash.SplashScreen
 
 @Composable
 fun Navigation(){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.Splash.route){
         composable(route = Screen.Splash.route){
-
+            SplashScreen(navController = navController)
         }
         composable(route = Screen.Welcome.route){
 
